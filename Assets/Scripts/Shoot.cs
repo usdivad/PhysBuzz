60716594b63b4a5f4eed23de461e6d5863357286
David Su <david.d.su@gmail.com>
2015-12-26 22:57:08 -0500

    add rocket + shoot scripts and prefabs

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	// rocket prefab
+	public GameObject rocketPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject g = (GameObject)Instantiate(rocketPrefab,
+			                                       transform.position, // exactly where the weapon is
+			                                       transform.parent.rotation); // face same direction player (weapon's parent) faces
+
+			// make rocket fly fwd by calling rigidbody's AddForce method
+			// (requires rocket to have rigidbody attached to it)
+			float force = g.GetComponent<Rocket>().speed;
+			g.GetComponent<Rigidbody>().AddForce(g.transform.forward * force);
+		}
+	}
+}

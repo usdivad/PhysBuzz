3d44d89c37cd0845ff3d2abb4ec296a83c8d77cf
David Su <david.d.su@gmail.com>
2015-12-30 23:00:41 -0500

    make eyeball retreat on collision; add weapon texture

--- Assets/Scripts/Eyeball.cs
+++ Assets/Scripts/Eyeball.cs
@@ -6,11 +6,15 @@ namespace PhysBuzz
 	public class Eyeball : MonoBehaviour {
 		public GameObject player;
 		public float speed;
+		private int maxRetreatTime;
+		private int curRetreatTime;
 
 		// Use this for initialization
 		void Start () {
 			player = GameObject.Find ("Player");
 			speed = 2.5f;
+			maxRetreatTime = 100;
+			curRetreatTime = 0;
 		}
 		
 		// Update is called once per frame
@@ -21,8 +25,13 @@ namespace PhysBuzz
 			Vector3 playerPos = player.transform.position; // player position
 			Vector3 dir = playerPos - transform.position; // target position relative to player
 			Vector3 movement = dir.normalized * speed * Time.deltaTime; // calculate movement based on speed
-			if (movement.magnitude > dir.magnitude) { // limit mvmt to never pass target position
-				movement = dir;
+			if (movement.magnitude > (dir.magnitude * 0.75)) { // limit mvmt to never pass target position
+				movement = transform.position;
+			}
+			if (curRetreatTime > 0) {
+				movement = movement * -2;
+
+				curRetreatTime = curRetreatTime - 1;
 			}
 			controller.Move (movement); // move it!
 
@@ -36,6 +45,11 @@ namespace PhysBuzz
 			float distance = Vector3.Distance (playerPos, transform.position);
 			OSCHandler.Instance.SendMessageToClient("ChucK", "/PhysBuzz/Eyeball", distance);
 
+		}
+
+		void OnCollisionEnter(Collision other) {
+			print ("ouch!");
+			curRetreatTime = maxRetreatTime;
 		}
 	}
 }
